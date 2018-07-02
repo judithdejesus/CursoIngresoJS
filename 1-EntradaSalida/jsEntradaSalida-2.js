6b30ad88e2f970aadc6a98b865970ecc12a56b61
judithdejesus <40763597+judithdejesus@users.noreply.github.com>
2018-07-02 12:06:22 -0300

    ejercicio 1 2 y 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,26 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	// comentarion de una linea
+
+	/* comentario 
+	de mas de una
+	lines*/
+    
+    /* la utlizacion del + sirve para dos cosas:
+    + de aritmetica (para una suma)
+    + de concatenacion (para unir un mesaje literal y una variable)
+    ejemplo:
+    nombre=prompt("El nombre ingresado es: "+nombre);*/
+
+    var nombre;
+    nombre=prompt(nombre);
+    alert (nombre);
+
+	/*var nombre = prompt("Ingrese su nombre", "Judith Judith");
+	la , se utliza para unir dos textos literal*/
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,14 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	alert ("Ejercicio 3");
+	console.log("Esto es un log");
+	*/
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+	alert (nombre);
 }
 
 
